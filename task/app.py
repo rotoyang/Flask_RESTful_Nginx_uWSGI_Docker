68b2b6894b0c7e123f28c1345a5d2855ec31acb0
Roto Yang <roto.yang@oakda.io>
2021-08-29 16:24:54 +0800

    revised for flake8

--- task/app.py
+++ task/app.py
@@ -8,18 +8,21 @@ app = create_app("development")
 db.init_app(app)
 migrate = Migrate(app, db)
 
+
 @app.route("/")
 def index():
     return "Rock'n Roll"
 
+
 if __name__ == "__main__":
     app.run()
+
 
 @app.cli.command()
 def test():
     import sys
     import unittest
-    
+
     tests = unittest.TestLoader().discover('tests')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.errors or result.failures:
--- task/config/config.py
+++ task/config/config.py
@@ -3,13 +3,14 @@ from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 def create_sqlite_uri(db_name):
     return "sqlite:///" + os.path.join(basedir, db_name)
 
 
 class BaseConfig:
     ADMIN_USER = "admin"
-    
+
 
 class DevelopmentConfig(BaseConfig):
     # Load environment variables
@@ -19,18 +20,19 @@ class DevelopmentConfig(BaseConfig):
     MYSQL_HOST = os.getenv('MYSQL_HOST')
     MYSQL_PORT = os.getenv('MYSQL_PORT')
     MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
-    
+
     DEBUG = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{}:{}@{}:{}/{}".format(MYSQL_USER, MYSQL_PASSWORD, MYSQL_HOST, MYSQL_PORT, MYSQL_DATABASE)
-    
-    
+
+
 class TestingConfig(BaseConfig):
     TESTING = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = create_sqlite_uri("test.db")
-    
+
+
 config = {
     "development": DevelopmentConfig,
     "testing": TestingConfig,
